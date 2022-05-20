218c7d1e1d711375ef7a0699ebd661d63b0b7ada
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-05-20 22:57:50 +0200

    Initial vulkan commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use vulkano::device::physical::PhysicalDevice;
+use vulkano::device::{Device, DeviceCreateInfo, Features, QueueCreateInfo};
+use vulkano::instance::{Instance, InstanceCreateInfo};
+
+fn main() {
+    println!("Hello, world!");
+
+    let instance = Instance::new(InstanceCreateInfo::default()).expect("failed to create instance");
+
+    let physical = PhysicalDevice::enumerate(&instance)
+        .next()
+        .expect("no device available");
+
+    for family in physical.queue_families() {
+        println!(
+            "Found a queue family with {:?} queue(s)",
+            family.queues_count()
+        );
+    }
+
+    let queue_family = physical
+        .queue_families()
+        .find(|&q| q.supports_graphics())
+        .expect("couldn't find a graphical queue family");
+
+    let (device, mut queues) = Device::new(
+        physical,
+        DeviceCreateInfo {
+            // here we pass the desired queue families that we want to use
+            queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
+            ..Default::default()
+        },
+    )
+    .expect("failed to create device");
+
+    let queue = queues.next().unwrap();
+}
