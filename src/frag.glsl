29a3db4f105417e7f47a2ac4b7e3ed7fc64aa7c5
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-05-28 22:53:21 +0200

    Jo

--- src/frag.glsl
+++ src/frag.glsl
@@ -2,6 +2,7 @@
 
 layout(location = 0) in vec3 v_normal;
 layout(location = 1) in vec2 tex_coords;
+
 layout(location = 0) out vec4 f_color;
 
 layout(set = 1, binding = 0) uniform sampler2D tex;
--- src/vert.glsl
+++ src/vert.glsl
@@ -5,6 +5,7 @@ layout(location = 1) in vec3 normal;
 layout(location = 2) in vec2 tex_coord;
 
 layout(location = 0) flat out vec3 v_normal;
+layout(location = 1) out vec2 v_tex_coord;
 
 layout(set = 0, binding = 0) uniform Data {
     mat4 world;
@@ -16,4 +17,5 @@ void main() {
     mat4 worldview = uniforms.view * uniforms.world;
     v_normal = transpose(inverse(mat3(worldview))) * normal;
     gl_Position = uniforms.proj * worldview * vec4(position, 1.0);
+    v_tex_coord = tex_coord;
 }
