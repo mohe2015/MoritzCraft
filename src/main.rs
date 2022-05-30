5a3723388be7a274063121dd6f565d5795d9fbbc
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-05-30 23:11:09 +0200

    small tests

--- src/main.rs
+++ src/main.rs
@@ -243,7 +243,7 @@ fn main() {
         2 * 3 + 2,
         3 * 3 + 2,
         2, // front
-       /* 4 * 3 + 2,
+        /* 4 * 3 + 2,
         5 * 3 + 2,
         6 * 3 + 2,
         6 * 3 + 2,
@@ -255,7 +255,7 @@ fn main() {
         0,
         4 * 3,
         7 * 3, // left
-       /* 3,
+        /* 3,
         2 * 3,
         5 * 3,
         2 * 3,
@@ -267,12 +267,12 @@ fn main() {
         3 + 1,
         4 * 3 + 1,
         5 * 3 + 1, // top
-        /*2 * 3 + 1,
-        6 * 3 + 1,
-        7 * 3 + 1,
-        2 * 3 + 1,
-        3 * 3 + 1,
-        7 * 3 + 1, // bottom*/
+                   /*2 * 3 + 1,
+                   6 * 3 + 1,
+                   7 * 3 + 1,
+                   2 * 3 + 1,
+                   3 * 3 + 1,
+                   7 * 3 + 1, // bottom*/
     ];
 
     // The start of this example is exactly the same as `triangle`. You should read the
@@ -380,9 +380,9 @@ fn main() {
     // For this example, we'll have the instances form a 10x10 grid that slowly gets larger.
     let instances = {
         let mut data = Vec::new();
-        for x in 0..1000 {
-            for y in 0..10 {
-                for z in 0..1000 {
+        for x in 0..100 {
+            for y in 0..1 {
+                for z in 0..100 {
                     data.push(InstanceData {
                         position_offset: [x as f32 * 20.0, y as f32 * 20.0, z as f32 * 20.0],
                     });
@@ -482,6 +482,22 @@ fn main() {
             } => {
                 recreate_swapchain = true;
             }
+            Event::WindowEvent {
+                event: WindowEvent::KeyboardInput { input, .. },
+                ..
+            } => {}
+            Event::WindowEvent {
+                event: WindowEvent::MouseInput { state, button, .. },
+                ..
+            } => {}
+            Event::WindowEvent {
+                event: WindowEvent::CursorMoved { position, .. },
+                ..
+            } => {}
+            Event::WindowEvent {
+                event: WindowEvent::MouseWheel { delta, .. },
+                ..
+            } => {}
             Event::RedrawEventsCleared => {
                 previous_frame_end.as_mut().unwrap().cleanup_finished();
 
