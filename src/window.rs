a2cf1bfd62be35575829df6b45c29c3a23e0cc5a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-05-31 19:49:45 +0200

    Jo

--- src/window.rs
+++ src/window.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2021 The vulkano developers
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or https://opensource.org/licenses/MIT>,
+// at your option. All files in the project carrying such
+// notice may not be copied, modified, or distributed except
+// according to those terms.
 use std::time::Instant;
 
 use crate::renderer::PoritzCraftRenderer;
@@ -23,15 +31,6 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
-
-// Copyright (c) 2021 The vulkano developers
-// Licensed under the Apache License, Version 2.0
-// <LICENSE-APACHE or
-// https://www.apache.org/licenses/LICENSE-2.0> or the MIT
-// license <LICENSE-MIT or https://opensource.org/licenses/MIT>,
-// at your option. All files in the project carrying such
-// notice may not be copied, modified, or distributed except
-// according to those terms.
 
 pub struct PoritzCraftWindow {}
 
