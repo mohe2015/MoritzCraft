f6df8bc51d3f4a7ed6d33c82c0284ebc8524959c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-06-15 00:29:28 +0200

    jo

--- src/worldgen.rs
+++ src/worldgen.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::data::{Block, Chunk};
 
 pub trait WorldGeneration {
@@ -8,10 +10,20 @@ pub struct RandomWorldGeneration {}
 
 impl WorldGeneration for RandomWorldGeneration {
     fn generate_chunk<C: Chunk>(&self, chunk: &mut Box<C>, chunk_x: usize, chunk_y: usize) {
+        let mut rng = rand::thread_rng();
         for x in 0..16 {
             for y in 0..16 {
                 for z in 0..16 {
-                    chunk.set_block(chunk_x + x, chunk_y + y, z, Some(Block::Dirt))
+                    chunk.set_block(
+                        chunk_x + x,
+                        chunk_y + y,
+                        z,
+                        match rng.gen_range(0..5) {
+                            0 => Some(Block::Dirt),
+                            1 => Some(Block::Stone),
+                            _ => None,
+                        },
+                    )
                 }
             }
         }
